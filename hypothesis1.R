f34cd570cbffecbe1a588aadd4d4d492d9646d89
shilpa2hait <53866771+shilpa2hait@users.noreply.github.com>
2021-01-11 16:02:11 +0530

    Add files via upload

--- hypothesis1.R
+++ hypothesis1.R
@@ -11,6 +11,52 @@ shapiro.test(Unit_B)
 
 var.test(Unit_A,Unit_B)
 
+#####Sample T Test#######
+
 t.test(Unit_A,Unit_B,alternative = "two.sided",conf.level = 0.95,correct=TRUE)
 ?t.test
 t.test(Unit_A,Unit_B,alternative = "greater")
+
+####On-way ANOVA####
+
+Cutlets1 <- read.csv(file.choose())
+attach(Cutlets1)
+View(Cutlets1)
+stack_Cutlets1 <- stack(Cutlets1)
+attach(stack_Cutlets1)
+annova_results <- aov(values~ind,data = stack_Cutlets1)
+summary(annova_results)
+
+###proportional T Test #####
+
+COF<-read.csv(file.choose())
+View(COF)
+attach(COF)
+
+table1 <- table(Phillippines,Indonesia,Malta,India)
+table1
+
+?prop.test
+prop.test(x=c(58,152),n=c(480,740),conf.level = 0.95,correct = FALSE,alternative = "two.sided")
+
+###Unequal proportion
+
+prop.test(x=c(58,152),n=c(480,740),conf.level = 0.95,correct = FALSE,alternative = "less")
+
+#### Chi-squre Test####
+
+BRO <- read.csv(file.choose())
+View(BRO)
+attach(BRO)
+table(Observed.Values,East, West, North, South)
+
+t2<-prop.table(table(East))
+t3<-prop.table(table(West))
+t4<-prop.table(table(North))
+t5<-prop.table(table(South))
+t1<-table(Observed.Values)
+
+chisq.test(table(Observed.Values,East))
+chisq.test(table(Observed.Values,West))
+chisq.test(table(Observed.Values,North))
+chisq.test(table(Observed.Values,South))
